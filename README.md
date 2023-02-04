c8ead61f2e247aea3ab359205c519ff2288f26d3
muhammedOzbilici <muhammed.ozbilici88@gmail.com>
2023-02-04 21:38:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Fersude
+# Don't waste, recycle
+This is a website for selling used items. Users can bid on items, buy them anytime with reasonable prices.
+
---
+++ order-service/src/test/java/com/fersude/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fersude.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
